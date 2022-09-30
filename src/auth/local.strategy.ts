fe8c443392745c733687689281f30fbca4c3a7fe
nhquyen2001 <nguyenhuuquyen2001@gmail.com>
2022-09-30 16:21:47 +0700

    comment note

--- src/auth/local.strategy.ts
+++ src/auth/local.strategy.ts
@@ -6,6 +6,7 @@ import { AuthService } from './auth.service';
 @Injectable()
 export class LocalStrategy extends PassportStrategy(Strategy) {
   constructor(private authService: AuthService) {
+    // override usernameField (usernameField -> email)
     super({ usernameField: 'email' });
   }
 
